ac7e3455669fe502f306017c590417ade7305afe
Danny Wilmer <wilmerdanny@users.noreply.github.com>
2021-08-10 01:56:30 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#creating a list of 3 lettered animal names
+animal_list=('cat', 'dog', 'pig', 'cow', 'fox', 'hog', 'bee')
+print(animal_list)
+print(animal_list[1])
+
+
---
+++ listmethods/listmeth01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+
+proto = ["ssh", "http", "https"]
+print(proto)
+
+
+print(proto[1])
+proto.extend("dns")
+print(proto)
+
+
+
