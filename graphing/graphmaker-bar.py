3e4e4af48606543762ce1fbc0b1adaa1b291bded
Danny Wilmer <wilmerdanny@users.noreply.github.com>
2021-08-12 17:59:23 +0000

    Learning to work with the matplotlib library

---
+++ graphing/graphmaker-bar.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg') 
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    N = 4
+    localnetMeans = (20, 35, 30, 35)
+    wanMeans = (25, 32, 34, 20)
+    ind = np.arange(N)
+    width = 0.35
+
+    # describe where to display p1
+    p1 = plt.bar(ind, localnetMeans, width)
+    # stack p2 on top of p1
+    p2 = plt.bar(ind, localnetMeans, width, bottom = localnetMeans)
+
+
+    # describe the table metadata 
+    plt.ylabel("Length of Outage (mins)")
+    plt.title("2018 Network Summary")
+    plt.xticks(ind, ("Q1", "Q2", "Q3", "Q4"))
+    plt.yticks(np.arange(0, 81, 10))
+    plt.legend((p1[0], p2[0]), ("LAN", "WAN",))
+
+
+    # display the graph
+    # plt.show() # you can try this on a python IDE with a GUI if you'd like
+    plt.savefig("/home/student/mycode/graphing/2018summary.png")
+    # save a copy to "~/static" (the "files" view) 
+    plt.savefig("/home/student/static/2018summary.png")
+
+
+
+
+if __name__ == "__main__":
+    main() 
