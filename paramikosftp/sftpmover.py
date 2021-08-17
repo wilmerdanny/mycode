cb168ecda43416de9f45a06b52998225fcca19f1
Danny Wilmer <wilmerdanny@users.noreply.github.com>
2021-08-17 15:42:17 +0000

    sftp with UN and PW

---
+++ paramikosftp/sftpmover.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3 
+
+# moving files with SFTP 
+
+# import paramiko so we can talk SSH 
+import paramiko
+import os 
+
+
+# where to connect to 
+t = paramiko.Transport("10.10.2.3", 22) ## IP and port
+
+# how to connect (see other labs on using id_rsa private/public keypairs)
+t.connect(username="bender", password="alta3")
+
+
+# make an sftp connetion object 
+sftp = paramiko.SFTPClient.from_transport(t)
+
+
+# iterate across files within the directory
+for x in os.listdir("/home/student/filestocopy/"):
+    if not os.path.isdir("/home/student/filestocopy/"+x): # find files, not dir
+        sftp.put("/home/student/filestocopy/"+x, "/tmp/"+x) # move file to target
+
+
+
+# close the connection 
+sftp.close()
