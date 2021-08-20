7ced64bcb40a447cc8d37866a26f99af1431405c
Danny Wilmer <wilmerdanny@users.noreply.github.com>
2021-08-20 16:40:06 +0000

    ready for prez

--- project/intraday_function.py
+++ project/intraday_function.py
@@ -7,16 +7,18 @@ import json
 import datetime 
 import os
 
-# predefining paths
-
+# predefining API key path
 creds_path  = "/home/student/alpha.creds"
 
 
-# getting api key from file 
+# creating functions for required parameters (symbol, interval, apikey)
+
+
+## function:  get "symbol" to use later and ensure compatibility 
 
 def get_symbol():
 
-    # capturing "symbol" variable
+    ### capturing "symbol" variable
     print("\nAPI function=TIME_SERIES_INTRADAY\n")
     print("Provide the symbol of the equity of your choice.") 
     print("For example: symbol=IBM")
@@ -31,17 +33,18 @@ def get_symbol():
         return symbol
 
 
+## function: get "interval" and  ensure compatibility
+
 def get_interval():
-
-    # getting interval 
-
+    ### instructing the user
     print("\nEnter desired time interval between two consecutive data points in the time series.")
     print("The following values are supported (in minutes):")   
     print("1, 5, 15, 30, 60")
     print("=================================================================")
-
+    ### getting input
     interval = int(input(">>>"))
-    # small check to help catch compatibility issues 
+    
+    ### small check to help catch compatibility issues 
     supported = [1, 5, 15, 30, 60]
     while interval not in supported:
         print("\nUnsupported value, try again.")
@@ -49,46 +52,40 @@ def get_interval():
     return interval
 
 
+## function: getting proper "API" and formatting it properly
+
 def get_api():
-    # getting apikey
+    ### getting apikey
     with open("/home/student/alpha.creds") as mycreds:
         alpha_creds = mycreds.read()
         apikey = "apikey=" + alpha_creds.strip("\n")
         return apikey
 
-
+## main loop
 cont = input("Continue? Y/n").lower()
 while cont == "y":
-    cont = input("Continue? Y/n").lower()
-
+    ### using symbol function
     symbol = get_symbol()
     print(symbol)
-
+    ### using  interval function
     interval = get_interval()
     print(interval)
-
+    ### using API key function
     alpha_creds = get_api()
     print(alpha_creds)
 
-
-
-
-
-
-    # getting intraday data from user input (above) in JSON format 
-    ## calling api
+    ### calling api
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}min&{alpha_creds}'
-    ## requesting data
+    ### requesting data
     r = requests.get(url)
-    ## reading data as JSON 
+    ### reading data as JSON 
     data = r.json()
 
 
-    # getting specified json key
+    ### getting specified json key
     key = data.get(f"Time Series ({interval}min)")
 
-    # displaying values from specified json key
-
+    ### displaying values from specified json key
     prices = []
     for ts in key:
         high_price = data[f'Time Series ({interval}min)'][ts]['2. high']
@@ -102,7 +99,7 @@ while cont == "y":
 
     save_or_not = input("Would you like to save output as file? Y/n").lower()
     if save_or_not == "y":
-        # saving output to file 
+        ##### saving output to file 
         contents = f"PRICES\n======\n{str(prices)}\nAVERAGE\n=======\n{str(average)}"
         save_path = f"/home/student/mycode/project/intraday_storage/{symbol}_{str(datetime.datetime.now())}" 
         with open(save_path, "w") as fp:
@@ -113,4 +110,5 @@ while cont == "y":
     else:
         print("Ok. No file has been saved.")
 
-
+    ### continute or exit loop
+    cont = input("Continue? Y/n").lower()
