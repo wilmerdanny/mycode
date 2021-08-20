608662033491e8504ce952aed9a2ad185eff7a23
Danny Wilmer <wilmerdanny@users.noreply.github.com>
2021-08-20 16:22:59 +0000

    Final

--- project/intraday_function.py
+++ project/intraday_function.py
@@ -5,24 +5,24 @@
 import requests 
 import json 
 import datetime 
+import os
+
+# predefining paths
+
+creds_path  = "/home/student/alpha.creds"
 
 
 # getting api key from file 
 
+def get_symbol():
 
-
-
-
-now = datetime.datetime.now()
-print(now)
-# capturing "symbol" variable
-print("\nAPI function=TIME_SERIES_INTRADAY\n")
-print("Provide the symbol of the equity of your choice.") 
-print("For example: symbol=IBM")
-print("=================================================================")
+    # capturing "symbol" variable
+    print("\nAPI function=TIME_SERIES_INTRADAY\n")
+    print("Provide the symbol of the equity of your choice.") 
+    print("For example: symbol=IBM")
+    print("=================================================================")
 
            
-def get_symbol():
     symbol = input("symbol=").upper()
     while len(symbol) > 4:
         print("\nInvalid symbol. Try again.")
@@ -31,19 +31,15 @@ def get_symbol():
         return symbol
 
 
-symbol = get_symbol()
-
-print(symbol)
-
-
-# getting interval 
-
-print("\nEnter desired time interval between two consecutive data points in the time series.")
-print("The following values are supported (in minutes):")   
-print("1, 5, 15, 30, 60")
-print("=================================================================")
-
 def get_interval():
+
+    # getting interval 
+
+    print("\nEnter desired time interval between two consecutive data points in the time series.")
+    print("The following values are supported (in minutes):")   
+    print("1, 5, 15, 30, 60")
+    print("=================================================================")
+
     interval = int(input(">>>"))
     # small check to help catch compatibility issues 
     supported = [1, 5, 15, 30, 60]
@@ -53,44 +49,68 @@ def get_interval():
     return interval
 
 
-
-
-interval = get_interval()
-print(interval)
-
 def get_api():
     # getting apikey
-    with open("/home/ec2-user/api/keys/alpha_vantage_pub") as mycreds:
+    with open("/home/student/alpha.creds") as mycreds:
         alpha_creds = mycreds.read()
         apikey = "apikey=" + alpha_creds.strip("\n")
         return apikey
 
-alpha_creds = get_api()
+
+cont = input("Continue? Y/n").lower()
+while cont == "y":
+    cont = input("Continue? Y/n").lower()
+
+    symbol = get_symbol()
+    print(symbol)
+
+    interval = get_interval()
+    print(interval)
+
+    alpha_creds = get_api()
+    print(alpha_creds)
 
 
-print(alpha_creds)
 
 
-# getting intraday data from user input (above) in JSON format 
-## calling api
-url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}min&{alpha_creds}'
-## requesting data
-r = requests.get(url)
-## reading data as JSON 
-data = r.json()
 
 
-# getting specified json key
-key = data.get(f"Time Series ({interval}min)")
+    # getting intraday data from user input (above) in JSON format 
+    ## calling api
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}min&{alpha_creds}'
+    ## requesting data
+    r = requests.get(url)
+    ## reading data as JSON 
+    data = r.json()
 
-# displaying values from specified json key
 
-prices = []
-for ts in key:
-    high_price = data[f'Time Series ({interval}min)'][ts]['2. high']
-    print(ts, high_price)
-    prices.append(float(high_price))
+    # getting specified json key
+    key = data.get(f"Time Series ({interval}min)")
 
-#print(prices)
-print("\nAverage:")
-print(sum(prices)/len(prices))
+    # displaying values from specified json key
+
+    prices = []
+    for ts in key:
+        high_price = data[f'Time Series ({interval}min)'][ts]['2. high']
+        print(ts, high_price)
+        prices.append(float(high_price))
+
+    print("\nAverage:")
+    average = (sum(prices)/len(prices))
+    print(average)
+
+
+    save_or_not = input("Would you like to save output as file? Y/n").lower()
+    if save_or_not == "y":
+        # saving output to file 
+        contents = f"PRICES\n======\n{str(prices)}\nAVERAGE\n=======\n{str(average)}"
+        save_path = f"/home/student/mycode/project/intraday_storage/{symbol}_{str(datetime.datetime.now())}" 
+        with open(save_path, "w") as fp:
+            fp.write(contents)
+            pass 
+
+        print(f"\nDone! output saved in file here:\n{save_path}")
+    else:
+        print("Ok. No file has been saved.")
+
+
