ef01d17c936858d47d52c53fe0cdddeebaabae3e
Danny Wilmer <wilmerdanny@users.noreply.github.com>
2021-08-20 03:24:02 +0000

    ALMOST DONE. added functions, works good. Add to file?

---
+++ project/intraday_function.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3 
+
+"""A script that calls time series closing value data from the Alpha Vantage API database. Closing values and the overall average value will be displayed. Data will then be saved in a file if desired. | wilmerdanny@icloud.com"""
+
+import requests 
+import json 
+import datetime 
+
+
+# getting api key from file 
+
+
+
+
+
+now = datetime.datetime.now()
+print(now)
+# capturing "symbol" variable
+print("\nAPI function=TIME_SERIES_INTRADAY\n")
+print("Provide the symbol of the equity of your choice.") 
+print("For example: symbol=IBM")
+print("=================================================================")
+
+           
+def get_symbol():
+    symbol = input("symbol=").upper()
+    while len(symbol) > 4:
+        print("\nInvalid symbol. Try again.")
+        symbol = input("symbol=").upper()
+    else:
+        return symbol
+
+
+symbol = get_symbol()
+
+print(symbol)
+
+
+# getting interval 
+
+print("\nEnter desired time interval between two consecutive data points in the time series.")
+print("The following values are supported (in minutes):")   
+print("1, 5, 15, 30, 60")
+print("=================================================================")
+
+def get_interval():
+    interval = int(input(">>>"))
+    # small check to help catch compatibility issues 
+    supported = [1, 5, 15, 30, 60]
+    while interval not in supported:
+        print("\nUnsupported value, try again.")
+        interval = int(input(">>>"))
+    return interval
+
+
+
+
+interval = get_interval()
+print(interval)
+
+def get_api():
+    # getting apikey
+    with open("/home/ec2-user/api/keys/alpha_vantage_pub") as mycreds:
+        alpha_creds = mycreds.read()
+        apikey = "apikey=" + alpha_creds.strip("\n")
+        return apikey
+
+alpha_creds = get_api()
+
+
+print(alpha_creds)
+
+
+# getting intraday data from user input (above) in JSON format 
+## calling api
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}min&{alpha_creds}'
+## requesting data
+r = requests.get(url)
+## reading data as JSON 
+data = r.json()
+
+
+# getting specified json key
+key = data.get(f"Time Series ({interval}min)")
+
+# displaying values from specified json key
+
+prices = []
+for ts in key:
+    high_price = data[f'Time Series ({interval}min)'][ts]['2. high']
+    print(ts, high_price)
+    prices.append(float(high_price))
+
+#print(prices)
+print("\nAverage:")
+print(sum(prices)/len(prices))
